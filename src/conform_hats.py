c0384757681a112b5dd021c6778775b37644a152
HimekaTsuchida <yuria.soft0615@gmail.com>
2024-06-15 18:18:29 +0900

    change

--- src/conform_hats.py
+++ src/conform_hats.py
@@ -1,5 +1,5 @@
 def please_conform(caps:str)-> None:
-    start = forward = backwoed = 0
+    start = forward = backward = 0
     intervals = []
     for i in range(1 , len(caps)):
         if caps[start] != caps[i]:
@@ -7,14 +7,14 @@ def please_conform(caps:str)-> None:
             if caps[start] == "F" :
                 forward += 1
             else:
-                backwoed += 1
+                backward += 1
             start = i
         intervals.append((start, len(caps)-1, caps[start]))
         if caps[start] == "F":
             forward += 1
         else:
-            backwoed += 1
-        flip = "F" if forward < backwoed else "B"
+            backward += 1
+        flip = "F" if forward < backward else "B"
         for t in intervals:
             if t[2] == flip:
                 print("People in positions", t[0], "through", t[1], "flip your caps!")
